17004518c933dec92ea03406b8c3dc1ad2825965
Mohmed Farouk <m.farouk@smartwave.com.eg>
2021-11-07 20:33:21 +0200

    device info

--- lib/WebViewPage/WebViewPage.dart
+++ lib/WebViewPage/WebViewPage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:nabd_mobile_app/utilites/size_config.dart';
+import 'package:device_info/device_info.dart';
 
 class WebViewScreen extends StatefulWidget {
 
@@ -12,8 +13,9 @@ class WebViewScreen extends StatefulWidget {
 }
 
 class _WebViewScreenState extends State<WebViewScreen> {
+  String? deviceId;
   bool isLoading = true;
-  String url ='https://survey.wowdesk.com/?r=survey/index&sid=361552&devId=devId';
+  String url= 'https://survey.wowdesk.com/?r=survey/index&sid=361552&devId=f5d4b025e2e20a91';
   // String url = 'https://flutter.dev';
   String finishedUrl = 'https://survey.wowdesk.com/?r=survey/index&sid=361552';
   final _key = UniqueKey();
@@ -47,11 +49,11 @@ class _WebViewScreenState extends State<WebViewScreen> {
               initialUrl: url,
               javascriptMode: JavascriptMode.unrestricted,
               onPageFinished: (url) {
-                if(url.toLowerCase() == finishedUrl){
-                  Future.delayed(const Duration(seconds: 20),(){
-                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> WebViewScreen()));
-                  });
-                }
+                // if(url.toLowerCase() == finishedUrl){
+                //   Future.delayed(const Duration(seconds: 20),(){
+                //     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> WebViewScreen()));
+                //   });
+                // }
                 setState(() {
                   isLoading = false;
                 });
@@ -64,11 +66,31 @@ class _WebViewScreenState extends State<WebViewScreen> {
       ),
     );
   }
+  getDeviceInfo() async{
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      setState(() {
+        deviceId = androidInfo.androidId;
+        url = 'https://survey.wowdesk.com/?r=survey/index&sid=361552&devId=$deviceId';
+        print(url);
+      });
 
+    }
+    else if(Platform.isIOS){
+      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      setState(() {
+        deviceId = iosInfo.identifierForVendor;
+        url = 'https://survey.wowdesk.com/?r=survey/index&sid=361552&devId=$deviceId';
+        print(url);
+      });
+    }
+  }
   @override
   void initState() {
     super.initState();
-    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+    getDeviceInfo();
   }
 
   @override
